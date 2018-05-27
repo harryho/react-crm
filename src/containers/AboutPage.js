370652e910f7970f977243d0f9424d791bf96bc1
harryho <harry.ho_long@yahoo.com>
2018-05-27 15:38:55 +1000

    Add github link

--- src/containers/AboutPage.js
+++ src/containers/AboutPage.js
@@ -32,7 +32,7 @@ class AboutPage extends React.Component {
         <div style={styles.version}>React CRM App Demo</div>
 
         <div style={styles.desc}>
-          <p>This demo app is not a real application. There is no fake API as back-end service behind the scene. Its advanced search doesn't work properly. Any data update (create or update record) will not be stored after hard refresh or logout. The main purpose of this demo is just a proof of concept.
+          <p>This demo app is not a real application. There is no fake API as back-end service behind the scene. The advanced search in the demo doesn't work properly. Any data update (create or update record) will not be stored after hard refresh or logout. The main purpose of this demo is just a proof of concept.
            </p>
         </div>
       </div>
