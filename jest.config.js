6543c77c13ef7ab192aa5a3b3abcabb693f210b8
harryho <hary.ho_long@yahoo.com>
2025-04-20 11:54:26 +1000

    b4 react mui uplift

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    transform: {
-      '.(ts|tsx)': 'ts-jest',
-    },
-    testPathIgnorePatterns: ['/node_modules/', '/lib/'],
-    testRegex: '(/test/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
-    };
